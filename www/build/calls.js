76a0878b9cf979807673b5bebb82c49870c648fa
FS-DK <frants@sandbergfamilien.dk>
2018-07-11 21:13:14 +0200

    Add files via upload

---
+++ www/build/calls.js
@@ -0,0 +1,9 @@
+$.ajax({
+  type: "GET",
+  url: "https://nyside.julecup.dk/wp-json/wp/v2/page?slug=turnering",
+  crossDomain: true,
+  cache: false,
+  success: function(result) {
+    $("#output").html(result[0].content.rendered)
+  }
+});
