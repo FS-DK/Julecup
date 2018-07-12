555240914ee77f7bcac60c2a153ff1b229c830fd
FS-DK <frants@sandbergfamilien.dk>
2018-07-12 14:50:32 +0200

    Update calls.js

--- www/build/calls.js
+++ www/build/calls.js
@@ -2,7 +2,7 @@ $.ajax({
   type: "GET",
   url: "https://nyside.julecup.dk/wp-json/wp/v2/page?slug=turnering",
   crossDomain: true,
-  cache: false,
+  cache: true,
   success: function(result) {
     $("#output").html(result[0].content.rendered)
   }
